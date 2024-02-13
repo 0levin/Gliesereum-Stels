b54fffa23f87028e5e31d52641c0e5c9d3faddc2
0leg <oleg@supremesoft.ru>
2024-02-13 17:18:47 +0300

    Updating slider items moving direction

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -142,7 +142,7 @@ export const Home = () => {
     
     var settings = {
         slidesToShow: 6,
-        slidesToScroll: -1,
+        slidesToScroll: 1,
         speed: 2000,
         autoplay: true,
         autoplaySpeed: 2000,
@@ -155,7 +155,7 @@ export const Home = () => {
             breakpoint: 1024,
             settings: {
                 slidesToShow: 3,
-                slidesToScroll: -1,
+                slidesToScroll: 1,
             }
         }],
     };
